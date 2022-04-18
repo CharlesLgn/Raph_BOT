08c00d1e49ee132b19bfec7c50580cf0b8eb7b59
RaphaelSch <raphael.scharff@gmail.com>
2022-04-18 10:49:48 +0200

    + SQL template

---
+++ template/raph_bot.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 18 avr. 2022 à 10:49
+-- Version du serveur :  10.5.12-MariaDB-0+deb11u1
+-- Version de PHP : 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `raph_bot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `alias_commands`
+--
+
+CREATE TABLE `alias_commands` (
+  `#` int(11) NOT NULL,
+  `UUID` varchar(36) NOT NULL,
+  `alias` varchar(30) NOT NULL,
+  `command` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commands`
+--
+
+CREATE TABLE `commands` (
+  `#` int(11) NOT NULL,
+  `UUID` varchar(36) NOT NULL,
+  `command` varchar(30) NOT NULL,
+  `text` text NOT NULL,
+  `auto` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `config`
+--
+
+CREATE TABLE `config` (
+  `#` int(11) NOT NULL,
+  `UUID` varchar(36) NOT NULL,
+  `id` varchar(30) NOT NULL,
+  `value` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `UUID` varchar(36) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `alias_commands`
+--
+ALTER TABLE `alias_commands`
+  ADD PRIMARY KEY (`#`);
+
+--
+-- Index pour la table `commands`
+--
+ALTER TABLE `commands`
+  ADD PRIMARY KEY (`#`);
+
+--
+-- Index pour la table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`#`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UUID`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `alias_commands`
+--
+ALTER TABLE `alias_commands`
+  MODIFY `#` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commands`
+--
+ALTER TABLE `commands`
+  MODIFY `#` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `config`
+--
+ALTER TABLE `config`
+  MODIFY `#` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
