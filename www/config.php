a933c51c5b879e78af74f67e8565e6d0f2d0287c
RaphaelSch <raphael.scharff@gmail.com>
2022-04-18 21:09:06 +0200

    Changed position of edit button

--- www/config.php
+++ www/config.php
@@ -25,7 +25,7 @@ while($row = mysqli_fetch_assoc($data)) {
     <tr>
         <td>".$row["id"]."</td>
         <td id='value_".$row["id"]."'>".$row["value"]."</td>
-        <td><button onClick='edit_entry(\"".$row["id"]."\", \"".$row["value"]."\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button></td>
+        <td><button onClick='edit_entry(\"".$row["id"]."\", \"".$row["value"]."\")' class='btn btn-warning pull-right' type='button'><i class='glyphicon glyphicon-pencil'></i></button></td>
     </tr>";
 }
 
