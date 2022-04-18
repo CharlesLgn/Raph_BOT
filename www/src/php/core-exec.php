d70a82c95710ee4543fbdf3fe598e7413eb768b1
RaphaelSch <raphael.scharff@gmail.com>
2022-04-18 12:40:52 +0200

    * core start correctly inside a screen

--- www/src/php/core-exec.php
+++ www/src/php/core-exec.php
@@ -1,12 +1,18 @@
 <?php 
 session_start();
 
-$core_directory = dirname(__DIR__)."/../../core/";
+$core_directory = dirname(__DIR__)."/../../core";
 $UUID = $_SESSION['UUID'];
 
 if(isset($_POST) && $_POST['action'] == "start"){
-    shell_exec('screen -dmS Raph_BOT-'.$UUID.' clear;  node '.$core_directory.'core.js '.$UUID.' > '.$core_directory.'logs/debug_'.$UUID.'.log 2>&1 &');
+    //node $core_directory/core.js $UUID > $core_directory/logs/debug_$UUID.log 2>&1
+
+    $command = "node $core_directory/core.js $UUID > $core_directory/logs/debug_$UUID.log 2>&1";
+
+    shell_exec("screen -dmS Raph_BOT-$UUID bash -c '$command'");
 }
+    
+    
 
 exit();
 ?>
